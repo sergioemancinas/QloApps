08be30c5626705f22ff29cd71931016cdd12996b
Sergio E Mancinas <sergioemancinas@MacBook-Pro.local>
2026-03-06 21:36:10 +0100

    Fix nav CMS links to avoid friendly-url 404s

--- modules/blocknavigationmenu/classes/WkCustomNavigationLink.php
+++ modules/blocknavigationmenu/classes/WkCustomNavigationLink.php
@@ -133,14 +133,6 @@ class WkCustomNavigationLink extends ObjectModel
                     $navigationLink['link'] = $interiorLink;
                 }
 
-                if (isset($navigationLink['id_cms']) && (int) $navigationLink['id_cms'] === 9) {
-                    $navigationLink['link'] = rtrim($context->link->getBaseLink((int) $context->shop->id, null, true), '/')
-                        .'/'.$context->language->iso_code.'/discover-lauscha';
-                } elseif (isset($navigationLink['id_cms']) && (int) $navigationLink['id_cms'] === 10) {
-                    $navigationLink['link'] = rtrim($context->link->getBaseLink((int) $context->shop->id, null, true), '/')
-                        .'/'.$context->language->iso_code.'/getting-here';
-                }
-
                 $filteredResult[] = $navigationLink;
             }
 
@@ -181,10 +173,7 @@ class WkCustomNavigationLink extends ObjectModel
                     'show_at_footer' => 0,
                     'active' => 1,
                     'position' => 9999 + $cmsId,
-                    'link' => in_array($cmsId, array(9, 10))
-                        ? rtrim($context->link->getBaseLink((int) $context->shop->id, null, true), '/')
-                            .'/'.$context->language->iso_code.'/'.($cmsId === 9 ? 'discover-lauscha' : 'getting-here')
-                        : $context->link->getCMSLink($cmsId),
+                    'link' => $context->link->getCMSLink($cmsId),
                     'name' => $cmsTitle,
                 );
             };
